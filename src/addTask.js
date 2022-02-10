4737cb19630e730fc191b02ccf21eaff32aad17a
flakster <carlos.santamaria@gmail.com>
2022-02-09 22:23:11 -0500

    Create addTAsk fuction

---
+++ src/addTask.js
@@ -0,0 +1,30 @@
+const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const addTask = async (event) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  const { name, description, hours, stack, role} = JSON.parse(event.body);
+  const id = v4()
+  const newTask = {
+    id,
+    name,
+    description,
+    hours,
+    stack,
+    role
+  }
+  await dynamodb.put({
+    TableName: "task",
+    Item: newTask
+  }).promise()
+
+  return {
+    status: 200,
+    body: {newTask}
+  }
+};
+
+
+module.exports = {
+  addTask,
+}
