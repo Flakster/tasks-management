2ec1950cb954ac678d05b222b6c8b8d35d8188f9
flakster <carlos.santamaria@gmail.com>
2022-02-16 15:43:51 -0500

    Add the option to assign the task to a new joiner

--- src/updateTask.js
+++ src/updateTask.js
@@ -3,15 +3,16 @@ const AWS = require('aws-sdk');
 const updateTask = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient({region:'us-east-1'})
   const {id} = event.pathParameters;
-  const {done} = JSON.parse(event.body)
+  const {done, newJoinerId} = JSON.parse(event.body)
   await dynamodb.update({
     TableName: "task",
     Key: {
       id
     },
-    UpdateExpression: 'set done = :done',
+    UpdateExpression: 'set done = :done, newJoinerId = :newJoinerId',
     ExpressionAttributeValues: {
-      ":done": done
+      ":done": done,
+      ":newJoinerId": newJoinerId
     },
     ReturnValues: 'ALL_NEW'
   }).promise()
